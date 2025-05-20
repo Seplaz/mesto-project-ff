4b58c52c4384d468458dc3fae446f528349eb668
Mikhail Karasev <seplaz38@yandex.ru>
2025-05-20 19:49:08 +0800

    validation in progress

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,6 +1,8 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
 };
 
 export function enableValidation() {
@@ -8,5 +10,5 @@ export function enableValidation() {
 };
 
 export function clearValidation() {
-  
+
 };
